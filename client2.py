e82bb8608298b927e65439ab8cd865caafeee81b
BennySkidanov <skidanov@post.bgu.ac.il>
2020-12-30 19:09:35 +0200

    save final

--- client2.py
+++ client2.py
@@ -1,55 +1,50 @@
 import socket
 import struct
 import sys
-import threading
 import tty, termios, sys
-from multiprocessing import Process
 import time
 import asyncio
-#from pynput.keyboard import Listener as keyBoardListener
-
 
 magic_cookie = 0xfeedbeef
 offer_msg_type = 0x2
 BUFFER_SIZE = 2048
 port = 13117
-team_name = """*** Python 3.8.5 (default, Jul 28 2020, 12:59:40) [GCC 9.3.0] on linux Type "help", "copyright", "credits" or "license" for more information. >>>         while True:File "<stdin>", line  while True: IndentationError: unexpected indent\n""".encode()
-
+team_name = "[Errno 111] Connection Refused\n".encode() # team name
 old_settings = None
 
 
-
 class Client:
-    def __init__(self):
+    def _init_(self):
+        # initiate client UDP socket - enbales reuse address and broadcast
         self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.client_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         self.client_socket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-        self.client_socket.bind(('172.1.255.255', port))
+        self.client_socket.bind(('172.1.255.255', port)) # recieve messages from entire subnet
 
     async def receive_msg(self):
 
         print("Client started, listening for offer requests...")
         while True:
             msg, server_address = self.client_socket.recvfrom(7)
-            (magicCookie, msg_type, server_port) = struct.unpack('!IbH', msg)
+            (magicCookie, msg_type, server_port) = struct.unpack('!IbH', msg) # 7 bytes, ! - big endian
             if magicCookie == magic_cookie:
                 if msg_type == 0x2:
-                    print("Received offer from " + str(server_address[0]) + ", attempting to connect...")
-                    print(server_address)
+                    print("Received offer from " + str(server_address[0]) + ", attempting to connect...") # recieved proper offer message (magic cookie and type)
+                    # print(server_address)
                     await self.connect_to_server(server_address, server_port)
 
     async def connect_to_server(self, server_address, server_connect_port):
-        print(server_connect_port)
-        try:
+        # print(server_connect_port)
+        try: # initiate a client TCP socket - again reuse addr, port and enable broadcast, and try to connect to the server
             client_socket_tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             client_socket_tcp.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
             client_socket_tcp.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
             client_socket_tcp.connect((server_address[0], server_connect_port))
             client_socket_tcp.send(team_name)
             modified_sentence = client_socket_tcp.recv(1024)
-            print("From Server: ", modified_sentence.decode())
+            print("From Server: ", modified_sentence.decode()) # recieve start game message from server
             fd = sys.stdin.fileno()
-            old_settings = termios.tcgetattr(fd)
+            old_settings = termios.tcgetattr(fd) # turn stdin.read to a non - blocking method using termios package
             try:
                 tty.setcbreak(sys.stdin.fileno())
             except:
@@ -63,11 +58,13 @@ class Client:
             print(e)
 
     async def handle_game(self, client_socket_tcp):
+        # create 2 taks ->  1. tWrite - read from keyboard and send to server
+        #                   2. tRead - recv from server and print
+        # we let them both run and the first one to end terminates both tasks
         tWrite = asyncio.create_task(self.get_char_from_user(client_socket_tcp))
         tRead = asyncio.create_task(self.recv_from_server(client_socket_tcp))
-        # tTimeout = asyncio.create_task(self.timeout_check())
         finished, unfinished = await asyncio.wait([tRead, tWrite], return_when = asyncio.FIRST_COMPLETED)
-        for task in unfinished:
+        for task in unfinished: # terminate all tasks that haven't been finished
             task.cancel()
 
 
@@ -82,10 +79,10 @@ class Client:
         loop = asyncio.get_event_loop()
         while True:
             try:
-                key = await loop.run_in_executor(None, lambda: sys.stdin.read(1))
+                key = await loop.run_in_executor(None, lambda: sys.stdin.read(1)) # read key from non blocking stdin
                 if not key == "":
                     print (key)
-                    client_socket_tcp.send(key.encode())
+                    client_socket_tcp.send(key.encode()) # send to server
             except Exception as e:
                 break
 
@@ -93,11 +90,11 @@ class Client:
         loop = asyncio.get_event_loop()
         while True:
             try:
-                modified_sentence = await loop.run_in_executor(None, lambda: client_socket_tcp.recv(1024))
-                if not modified_sentence:
+                modified_sentence = await loop.run_in_executor(None, lambda: client_socket_tcp.recv(1024)) # recv from server
+                if not modified_sentence: # empty msg = server finished the game and closed the connection
                     print("SERVER TOLD ME GAME IS OVER :( ")
                     break
-                print (modified_sentence)
+                print (modified_sentence.decode())
             except:
                 print("SERVER TOLD ME GAME IS OVER :( ")
                 break
--- server.py
+++ server.py
@@ -1,19 +1,17 @@
-import multiprocessing
 import threading
 import socket
 import struct
 import time
-from timeit import Timer
-from multiprocessing import Process
 
-import select
-import scapy.all as scapy
+import scapy.all as scapy # scapy to use IP
 
+# colors for bonus
 CYAN = '\u001b[36m'
 YELLOW = '\u001b[33m'
 BLUE =  '\u001b[34m'
 RESET= '\u001b[0m'
-
+RED = '\u001b[31m'
+GREEN = '\u001b[32m'
 
 
 
@@ -31,6 +29,8 @@ group1 = []
 group2 = []
 teams_counters ={}
 
+best_team_ever = [None,-1] # we save the best teams as a cool stat :)
+
 start_message_part1 = """Welcome to Keyboard Spamming Battle Royale.
 Group 1:
 ==
@@ -46,7 +46,8 @@ Start pressing keys on your keyboard as fast as you can!!
 
 class Server:
 
-    def __init__(self):
+    def _init_(self):
+        # initiate server UDP socket - enbales reuse address and broadcast
         self.server_udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.server_udp_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         self.server_udp_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
@@ -70,16 +71,16 @@ class Server:
 
         while True:
             now = time.time()
-            if now-start > 10:
+            if now-start > 10: # we mesure time and close offer sending state after 10 seconds
                 break
             try:
-
+                # initiate a server TCP socket - again reuse addr, port and enable broadcast, and listen for client that supposed to connect
                 server_socket_tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
                 server_socket_tcp.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
                 server_socket_tcp.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
                 server_socket_tcp.settimeout(10)
-                server_socket_tcp.bind((server_ip, 0))
-                tcp_port = server_socket_tcp.getsockname()[1]
+                server_socket_tcp.bind((server_ip, 0)) # 0 -> generate free port
+                tcp_port = server_socket_tcp.getsockname()[1] # get the free port allocated by OS
                 server_socket_tcp.listen(1)
                 udp_lock.acquire()
                 udp_connection_thread= threading.Thread(target=self.wait_for_accept_offer,
@@ -89,40 +90,41 @@ class Server:
                 client_socket_tcp, client_address = server_socket_tcp.accept()
                 udp_lock.release()
 
-                name = client_socket_tcp.recv(2048)
-                print(BLUE + "Willkommen zuhause: " + name.decode() + RESET)
-                print("GIT RDY!!!")
-                teams.append((name, client_socket_tcp))
+                name = client_socket_tcp.recv(2048) # recieve team name from client
+                print(BLUE + "Willkommen zuhause: " + name.decode() + RESET) # a litlle German :)
+                #print("GIT RDY!!!")
+                teams.append((name, client_socket_tcp)) # add new team to our data structure
                 teams_counters[name]=0
                 # client_socket_tcp.send(b"you are connected, wait for game to start")
-                print("waiting for more clients")
+                # print("waiting for more clients")
 
             except Exception as e:
                 udp_lock.release()
                 continue
 
         time.sleep(2)
-        print("Starting Game...")
+        print(RED + "Initiating Game Protocol..." + RESET)
         self.handle_game()
 
     def wait_for_accept_offer(self,server_port):
         while udp_lock.locked():
-            msg = struct.pack('!IbH', magic_cookie, offer_msg_type, server_port)
-            self.server_udp_socket.sendto(msg, ('172.1.255.255', server_broadcast_port))
+            msg = struct.pack('!IbH', magic_cookie, offer_msg_type, server_port) # 7 bytes, ! - big endian
+            self.server_udp_socket.sendto(msg, ('172.1.255.255', server_broadcast_port)) # send broadcast offer to all subnet
 
 
     def handle_game(self):
+        # handle game
         self.assign_to_groups()
         threads = []
-        for team in teams:
+        for team in teams: # create thread for each client / team
             thread = threading.Thread(target=self.handle_game_single_client,
                                       args=(team,))
             threads.append(thread)
             thread.start()
         for x in threads:
-            x.join()
+            x.join() # wait for all clients to finish
 
-        g1_counter = 0
+        g1_counter = 0 # group counters
         g2_counter = 0
         for group in teams_counters.keys():
             if group in group1:
@@ -130,42 +132,44 @@ class Server:
             else:
                 g2_counter+=teams_counters[group]
 
-        winner = (group1,1) if g1_counter>g2_counter else (group2,2)
-        print("""Game over !
+        winner = (group1,1) if g1_counter>g2_counter else (group2,2) # generate game summary msg
+        end_messsage = """Game over !
         Group 1 typed in %i characters.
         Group 2 typed in %i characters.
         Group %i wins!
         Congratulations to the winners:
-        =="""%(g1_counter, g2_counter, winner[1]))
+        ==
+        """%(g1_counter, g2_counter, winner[1])
 
 
         for x in winner[0]:
-            print(x.decode())
+            end_messsage += (GREEN + x.decode())
+
+        end_messsage += YELLOW + "some fun statistics:"
+        end_messsage += BLUE + """best team to ever play in this server are the legendaries %s,
+                and their score was: %d"""  %(best_team_ever[0],best_team_ever[1]) + RESET
+
+        print(end_messsage)
+
+        for team in teams:
+            try:
+                team[1].send(end_messsage.encode())
+            except:
+                continue
 
 
+        for team in teams:
+            try:
+                team[1].close()
+            except:
+                continue
 
-
-        # print(start_message_part1 + self.to_string_group(group1) + start_message_part2 +
-        #    self.to_string_group(group2) + start_message_part3)
-
-        # c.release()
-        # c.notify_all()
 
     def handle_game_single_client(self, tup):
-        # while game_lock.locked():
-        #     print("Thread " + str(threading.current_thread().ident) + " is going night night")
-        #     time.sleep(1)
-        #
-        # print("Thread " + str(threading.current_thread().ident) + " is NOT going night night")
         group_counter = 0
         tup[1].send((BLUE + start_message_part1 + self.to_string_group(group1) + start_message_part2 +
                      self.to_string_group(group2) + start_message_part3 +RESET).encode())
 
-        # c = tup[1].recv(1)
-        # team_counter+=1
-        # print(tup[0] + b" " + str.encode(str(team_counter)))
-        # tup[1].send(b"KIBALTI ET HATAV YA KAKI")
-        # ready = select.select([tup[1]], [], [],10)
         tup[1].setblocking(0)
         start = time.time()
 
@@ -174,21 +178,25 @@ class Server:
             if now - start > 10:
                 break
             try:
-                c = tup[1].recv(3)
-                group_counter+=1
-                tup[1].send("KIBALTI ET HATAV YA KAKI").encode()
+                c = tup[1].recv(3) # recv char and increase counter
+                if c:
+                    group_counter+=1
             except:
                 continue
 
         # tup[1].send(b"stop")
-        dict_lock.acquire()
+        dict_lock.acquire() # lock and update statistics
         teams_counters[tup[0]] = group_counter
+        if group_counter>best_team_ever[1]:
+
+            best_team_ever[0] = tup[0].decode()
+            best_team_ever[1]= group_counter
         dict_lock.release()
         #print("group counter " + (str(group_counter)))
-        tup[1].close()
+        #tup[1].close()
 
 
-    def assign_to_groups(self):
+    def assign_to_groups(self): # make interleaving group assigning function
         group1bool = True
         for i in range(0, len(teams)):
             if group1bool:
@@ -198,7 +206,7 @@ class Server:
                 group2.append((teams[i][0]))
                 group1bool = True
 
-    def to_string_group(self, group):
+    def to_string_group(self, group): # print group name from data structure
         ret_str = ""
         for g in group:
             ret_str = ret_str + (str(g.decode()))
